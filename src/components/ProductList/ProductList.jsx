f800f259be0902010db07364402bfeedab9eb00a
SIDDXSingh <siddharth12341234@gmail.com>
2023-07-18 22:48:33 +0530

    2nd commit
    Signed-off-by: SIDDXSingh <siddharth12341234@gmail.com>

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ProductCards from "../ProductCards/ProductCards";
+import {useState} from "react";
+
 
 import styles from "./ProductList.module.css";
 
@@ -27,17 +29,36 @@ var prodLis=[
     }
 ]
 
-
+function getProductsApi(callback)
+{
+    console.log("API called");
+    setTimeout(()=>{callback(prodLis)},3000);
+}
 
 
 function ProductList()
 {
+    const[isLoading,setIsLoading]=useState(true);
+    const[prodLis,setProdLis]=useState([]); //prodLis is a state variable and setProdLis is a function to set the state variable
+    useEffect(()=>{getProductsApi((prodLis)=>{setIsLoading(false);
+                                    setProdLis(prodLis)})},[]);
+
+
+    if(isLoading)
+    {
+        
+        return <div>
+            <>Loading</>
+            </div>
+
+    }
+    else{
     return (
         <div class={styles.list} >
             {
                 prodLis.map((prodLis)=>{return <ProductCards title={prodLis.title} price={prodLis.price}/>  })
             }
         </div>);
-}
+}}
 
 export default ProductList
